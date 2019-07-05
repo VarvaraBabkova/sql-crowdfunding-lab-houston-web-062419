6047952c197f7b75582f24a393d5e9122cac5d4f
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 14:59:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,9 +27,9 @@ group by users.name order by sum(pledges.amount), users.name; "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+""
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+""
 end

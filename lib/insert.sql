348032169aa0d75d752251c2a9ff8bae4a5f5ab2
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 14:28:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,4 @@
 
--- INSERT INTO users VALUES (1, "User 1", 20);
--- INSERT INTO users VALUES (2, "User 2", 21);
--- INSERT INTO users VALUES (3, "User 3", 22);
--- INSERT INTO users VALUES (4, "User 4", 23);
--- INSERT INTO users VALUES (5, "User 5", 24);
-
 create trigger users_ins_trigger after insert on users
   when new.id < 20 begin
     insert into users(id, name, age) values(new.id + 1, "user " + new.id, 20 + new.id);

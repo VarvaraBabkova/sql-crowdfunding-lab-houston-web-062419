53d7ec47cca3a82d987a527d6cfde753a7e01946
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 13:37:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT,
-category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+            category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
-CREATE TABLE pledge (id INTEGER PRIMARY KEY, );
+CREATE TABLE pledge (id INTEGER PRIMARY KEY, amount INTEGER,
+            user_id INTEGER, project_id INTEGER);

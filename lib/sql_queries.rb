08af23d35325c99e07f053d9426cbb7365defc70
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 14:54:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ where pledges.user_id = users.id group by users.name order by users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"select projects.title, (projects.funding_goal - sum(pledges.amount)) from projects, pledges
+"select projects.title, (sum(pledges.amount)) from projects, pledges
 where projects.id = pledges.project_id group by projects.title having sum(pledges.amount) >= projects.funding_goal;"
 end
 

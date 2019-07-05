796ce89f72bab64104799e49835d94617e9d67c0
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 14:41:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,8 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "select projects.title, pledges.amount from projects, pledges where projects.id = pledges.project_id order by projects.title;"
+  "select projects.title, sum(pledges.amount) from projects, pledges 
+  where projects.id = pledges.project_id order by projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
